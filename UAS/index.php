df6b7c7742885bfcde3f4c7f036600812cb689d7
MasGent <90071441+MasGent@users.noreply.github.com>
2022-05-30 00:22:56 +0700

    Add files via upload

---
+++ UAS/index.php
@@ -0,0 +1,12 @@
+<?php
+$a=3;
+$b=4;
+$c="AKU";
+$d="AKU";
+
+ printf("$a < $b --> %d <br>",$a<$b);
+ printf("$a > $b --> %d <br>",$a>$b);
+ printf("$c = $d --> %d <br>",$c==$d);
+ printf("$c != $d --> %d <br>",$c!=$d);
+ print("12 % 4 = ");print(12%4);
+?>
